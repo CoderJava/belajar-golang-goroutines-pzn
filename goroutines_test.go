f8060f3dfc7720d6fb0205b6444c77f3bb3f23a5
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-06-07 21:10:18 +0700

    Pembuktian goroutine sangat ringan

--- goroutines_test.go
+++ goroutines_test.go
@@ -15,3 +15,16 @@ func TestCreateGoroutine(t *testing.T) {
 	fmt.Println("Ups")
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Number", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	limit := 100000
+	// limit := 20
+	for number := 1; number <= limit; number++ {
+		go DisplayNumber(number)
+	}
+	time.Sleep(5 * time.Second)
+}
